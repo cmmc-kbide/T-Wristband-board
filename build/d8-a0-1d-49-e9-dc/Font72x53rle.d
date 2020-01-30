1a3bfa289f1d715e6ecdccd52f9369b778dc1c1a
bavensky <apiruk_sang-ngenchai@hotmail.com>
2020-01-30 14:40:37 +0700

    remove build file

--- build/d8-a0-1d-49-e9-dc/Font72x53rle.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Asus\AppData\Local\Programs\kbide\boards\ttgo-t-wristband\build\d8-a0-1d-49-e9-dc\Font72x53rle.o: \
- C:\Users\Asus\AppData\Local\Programs\kbide\boards\ttgo-t-wristband\include\Fonts\Font72x53rle.c \
- C:\Users\Asus\AppData\Local\Programs\kbide\platforms\arduino-esp32\lib\include/pgmspace.h
